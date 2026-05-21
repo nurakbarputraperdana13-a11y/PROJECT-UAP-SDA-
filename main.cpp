bdedd15afda406cadd56079c0d03cb7a33866270
unknown <nurakbarputraperdana13@gmail.com>
2026-05-21 15:46:10 +0700

    penambahan output

--- main.cpp
+++ main.cpp
@@ -10,6 +10,14 @@ class Produk {
         int stok;
 
     public:
+        
+        Produk (){
+            id = 0;
+            nama = "";
+            harga = 0;
+            stok = 0;
+        }
+        
         Produk (int id, string nama, int harga, int stok){
             this->id = id;
             this->nama = nama;
@@ -49,11 +57,10 @@ class Produk {
             return stok;
         }
 
-
-        void tampilkan(){
-            cout << "Id produk: " << getNama() << endl;
-        }
 };
+
+
+
 
 
 
@@ -61,42 +68,89 @@ class Produk {
 
 int main (){
 
-    int pilihan;
 
+    cout << "Selamat datang di Toko Sembako!" << endl;
+    cout << "         Masuk Sebagai         " << endl;
+    cout << "1. Admin" << endl;
+    cout << "2. Pembeli" << endl;
+    cout << "Pilih peran: ";
+    int peran;
+    cin >> peran;
+
+
+    if (peran == 1) {
+        cout << "Anda masuk sebagai Admin" << endl;
     do {
-    cout << "1. Login" << endl;
-    cout << "2. Register" << endl;
-    cout << "3. keluar" << endl;
-    cout << "Pilih (angka): ";
-    cin >> pilihan;
-    cout << endl;
+    int pilihan;
+        cout << "----Menu----" << endl;
+        cout << "1. Tambah Produk" << endl;
+        cout << "2. Tampilkan Semua Produk" << endl;
+        cout << "3. Cari Produk" << endl;
+        cout << "4. Proses pesanan" << endl;
+        cout << "0. Kembali" << endl;
+        cout << "Pilih menu: ";
+        cin >> pilihan;
 
-    if (!(cin >> pilihan)){
-        cin.clear();
-        cin.ignore(1000,'\n');
-        cout << "Input harus angka!\n" << endl;
-        continue;
+        switch (pilihan) {
+            case 1:
+                cout << "Tambah Produk" << endl;
+                break;
+            case 2:
+                cout << "Tampilkan Semua Produk" << endl;
+                break;
+            case 3:
+                cout << "Cari Produk" << endl;
+                break;
+            case 4:
+                cout << "Proses pesanan" << endl;
+                break;
+            case 0:
+                cout << "Kembali" << endl;
+                break;
+            default:
+                cout << "Pilihan tidak valid" << endl;
+        }
+    } while (true);
+
+    } else if (peran == 2) {
+        cout << "Anda masuk sebagai Pembeli" << endl;
+        do {
+
+    int pilihan;
+        cout << "----Menu----" << endl;
+        cout << "1. Tampilkan Semua Produk" << endl;
+        cout << "2. Cari Produk" << endl;
+        cout << "3. Checkout Produk" << endl;
+        cout << "0. Keluar" << endl;
+        cout << "Pilih menu: ";
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1:
+                cout << "Tampilkan Semua Produk" << endl;
+                break;
+            case 2:
+                cout << "Cari Produk" << endl;
+                break;
+            case 3:
+                cout << "Checkout Produk" << endl;
+                break;
+            case 0:
+                cout << "Kembali" << endl;
+                break;
+            default:
+                cout << "Pilihan tidak valid" << endl;
+        }
+    } while (true);
+    
+    } else if (peran == 0) {
+        cout << "Terima kasih telah menggunakan aplikasi Toko Sembako!" << endl;
+        return 0;
+    }else {
+        cout << "Pilihan tidak valid" << endl;
     }
-
-    switch (pilihan)
-    {
-    case 1:
-
-        
-
-        break;
-    case 2:
-
-        break;
-
-    default:
-        continue;
-        break;
-    }
-
-
-
-}while (pilihan !=3);
 
     return 0;
 }
+
+
